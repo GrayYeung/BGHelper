19941ae10902baa3b98f73f62c6bdf9c4e8a9f42
gray.yy.yeung <gray.yy.yeung@gtomato.com>
2021-05-25 15:39:41 +0800

    Testing: added another React FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Component1 from "./components/Component1";
+import Component2 from "./components/Component2";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <Component1/>
+        <Component2/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Component2.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Component2: React.FC = () => {
+    return (
+        <h1>Title from C2</h1>
+    )
+}
+
+export default Component2
